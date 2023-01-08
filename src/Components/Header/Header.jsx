01f95e6386f1539ca07c2e60ed7a1a4a52c202c5
Md. Shamrat Hossain <shamratpg@gmail.com>
2023-01-08 11:15:44 +0600

    added simple nav bar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './Header.scss'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='navbar'>
+            <div className="left">
+                <div className="item">
+                    <select className='select-item'>
+                        <option>USD</option>
+                        <option>CAD</option>
+                        <option>AUD</option>
+                    </select>
+                </div>
+                <div className="item">
+                    <Link className='link' to='/product/1'>Women</Link>
+                </div>
+                <div className="item">
+                    <Link className='link' to='/product/2'>Men</Link>
+                </div>
+                <div className="item">
+                    <Link className='link' to='/product/3'>Children</Link>
+                </div>
+            </div>
+            <div className="center">
+                <div className="item">
+                    <Link className='link' to="./">Magic Purchase</Link>
+                </div>
+            </div>
+            <div className="right">
+                <div className="item">
+                    <Link className="link" to='./'> Home</Link>
+                </div>
+                <div className="item">
+                    <Link className="link" to='/about'> About</Link>
+                </div>
+                <div className="item">
+                    <Link className="link" to='/contact'> Contact</Link>
+                </div>
+                <div className="item">
+                    <Link className="link" to='/store'> Store</Link>
+                </div>
+                <div className="item">
+                    <div className="icons">
+                        <div className="icon">
+
+                            <SearchIcon />
+                        </div>
+                        <div className="icon">
+                            <AccountCircleIcon />
+                        </div>
+                        <div className="icon">
+                            <FavoriteBorderIcon />
+
+                        </div>
+                        <div className="cart icon">
+                            <ShoppingCartIcon />
+                            <span className='cart-number'>
+                                0
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
