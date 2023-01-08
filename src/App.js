c439a4855c8ce7c21dbd78d751969a700a62f19f
Md. Shamrat Hossain <shamratpg@gmail.com>
2023-01-08 14:01:53 +0600

    added responsive features to the nav bar item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import './App.scss';
 import router from './Router/Router';
 
 function App() {
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
 import './Header.scss'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+    const [isLeftActive, setIsLeftActive] = useState(false)
+    const dropMenu = (menu) => {
+        if (menu === 'left') {
+            setIsLeftActive(!isLeftActive)
+        }
+    }
     return (
         <div className='navbar'>
             <div className="left">
-                <div className="item">
-                    <select className='select-item'>
-                        <option>USD</option>
-                        <option>CAD</option>
-                        <option>AUD</option>
-                    </select>
+                <div className="menu-icon">
+                    <MenuIcon onClick={() => dropMenu('left')} />
                 </div>
-                <div className="item">
-                    <Link className='link' to='/product/1'>Women</Link>
-                </div>
-                <div className="item">
-                    <Link className='link' to='/product/2'>Men</Link>
-                </div>
-                <div className="item">
-                    <Link className='link' to='/product/3'>Children</Link>
+                <div className={isLeftActive ? "menu-items" : 'desktop-menu'}>
+                    <div className="item">
+                        <select className='select-item'>
+                            <option className='select-option'>USD</option>
+                            <option className='select-option'>CAD</option>
+                            <option className='select-option'>AUD</option>
+                        </select>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to='/products/1'>Women</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to='/products/2'>Men</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to='/products/3'>Children</Link>
+                    </div>
                 </div>
             </div>
             <div className="center">
@@ -33,36 +45,37 @@ const Header = () => {
                 </div>
             </div>
             <div className="right">
-                <div className="item">
-                    <Link className="link" to='./'> Home</Link>
+                <div className="menu-icon">
+                    <MenuIcon />
                 </div>
-                <div className="item">
-                    <Link className="link" to='/about'> About</Link>
-                </div>
-                <div className="item">
-                    <Link className="link" to='/contact'> Contact</Link>
-                </div>
-                <div className="item">
-                    <Link className="link" to='/store'> Store</Link>
-                </div>
-                <div className="item">
-                    <div className="icons">
-                        <div className="icon">
+                <div className="menu-items">
+                    <div className="item">
+                        <Link className="link" to='./'> Home</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to='/about'> About</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to='/contact'> Contact</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to='/store'> Store</Link>
+                    </div>
+                    <div className="item">
+                        <div className="icons">
+                            <div className="icon">
 
-                            <SearchIcon />
-                        </div>
-                        <div className="icon">
-                            <AccountCircleIcon />
-                        </div>
-                        <div className="icon">
-                            <FavoriteBorderIcon />
-
-                        </div>
-                        <div className="cart icon">
-                            <ShoppingCartIcon />
-                            <span className='cart-number'>
-                                0
-                            </span>
+                                <SearchIcon />
+                            </div>
+                            <div className="icon">
+                                <AccountCircleIcon />
+                            </div>
+                            <div className="cart icon">
+                                <ShoppingCartIcon />
+                                <span className='cart-number'>
+                                    0
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
